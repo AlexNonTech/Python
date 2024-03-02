f07edbadcacd06fa6a00b58b47df9c6c14b8973e
AlexNonTech <AlexZinTech@gmail.com>
2024-03-02 22:26:57 +0200

    Hash_Tables
    Learning about Hash Tables :)

---
+++ Data Structures & Algorithms/hash_tables.py
@@ -0,0 +1,11 @@
+hash_table = {}
+hash_table['age'] = 18
+hash_table['name'] = "AlexTech"
+hash_table['height'] = 188
+
+for key in hash_table:
+    print(hash_table[key])
+
+for key, value in enumerate(hash_table):
+    print(key, value)
+
