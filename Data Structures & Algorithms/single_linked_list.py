3d88b5046b29604df2c1d39ac224e590b75986fb
AlexNonTech <AlexZinTech@gmail.com>
2024-03-03 10:20:29 +0200

    SingleLinkedLists

---
+++ Data Structures & Algorithms/single_linked_list.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete_node(self, key):
+        current_node = self.head
+        if current_node and current_node.data == key:
+            self.head = current_node.next
+            current_node = None
+            return
+        prev = None
+        while current_node and current_node.data != key:
+            prev = current_node
+            current_node = current_node.next
+        if current_node is None:
+            return
+        prev.next = current_node.next
+        current_node = None
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+        print("None")
+
+
+if __name__ == "__main__":
+    linked_list = SingleLinkedList()
+    linked_list.append(30)
+    linked_list.append(40)
+    linked_list.append(10)
+    linked_list.append(25)
+    linked_list.print_list()
+    linked_list.prepend(5)
+    linked_list.print_list()
+    linked_list.delete_node(10)
+    linked_list.print_list()
