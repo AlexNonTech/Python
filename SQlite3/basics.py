e98d3860e5f26251bbb0d35ebfdb3e7e1086b752
AlexNonTech <AlexZinTech@gmail.com>
2024-03-12 13:39:01 +0200

    basics

---
+++ SQlite3/basics.py
@@ -0,0 +1,29 @@
+import sqlite3 as sq
+
+with sq.connect('base.db') as con:
+    cur = con.cursor()
+
+    # cur.execute(" DROP TABLE IF EXISTS basics ")
+    cur.execute(""" CREATE TABLE IF NOT EXISTS basics (
+    id INTEGER,
+    user TEXT NOT NULL,
+    score INTEGER DEFAULT 0
+    )""")
+
+    #cur.execute("INSERT INTO basics VALUES (1, 'Mason', 135)")
+
+    cur.execute("SELECT * FROM basics")
+    entire_db = cur.fetchall()
+    print(entire_db)
+
+    cur.execute("SELECT * FROM basics WHERE user=='Suzy'")
+    filtered_name = cur.fetchall()
+    print(filtered_name)
+
+    cur.execute("SELECT * FROM basics ORDER BY score DESC ")
+    ordered_exe = cur.fetchall()
+    print(ordered_exe)
+
+    cur.execute("SELECT * FROM basics WHERE score BETWEEN 100 AND 150 ORDER By score DESC ")
+    filter_score = cur.fetchall()
+    print(filter_score)
