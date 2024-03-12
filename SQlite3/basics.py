1e5878111abf87f4d757c3afae8c374b81c6000f
AlexNonTech <AlexZinTech@gmail.com>
2024-03-12 14:03:13 +0200

    basics

--- SQlite3/basics.py
+++ SQlite3/basics.py
@@ -3,14 +3,19 @@ import sqlite3 as sq
 with sq.connect('base.db') as con:
     cur = con.cursor()
 
-    # cur.execute(" DROP TABLE IF EXISTS basics ")
+    #cur.execute(" DROP TABLE IF EXISTS basics ")
     cur.execute(""" CREATE TABLE IF NOT EXISTS basics (
     id INTEGER,
     user TEXT NOT NULL,
     score INTEGER DEFAULT 0
     )""")
 
-    #cur.execute("INSERT INTO basics VALUES (1, 'Mason', 135)")
+    data = []
+    names = ['ALex', 'Suzy', 'Stacy', 'Jason', 'Michael']
+    for i in range(5):
+        data.append((i + 1, names[i], 100*(i + 1)))
+
+    cur.executemany("INSERT INTO basics VALUES (?, ?, ?)", data)
 
     cur.execute("SELECT * FROM basics")
     entire_db = cur.fetchall()
@@ -27,3 +32,5 @@ with sq.connect('base.db') as con:
     cur.execute("SELECT * FROM basics WHERE score BETWEEN 100 AND 150 ORDER By score DESC ")
     filter_score = cur.fetchall()
     print(filter_score)
+
+    con.commit()
